4722b2a913399b6411c0a52314c71b7920a4a9a4
SmalecBartek <smalecbartlomiej@gmail.com>
2018-10-21 00:11:57 +0200

    init

--- knn.py
+++ knn.py
@@ -1,10 +1,31 @@
+import pandas as pd
+import numpy as np
+from scipy.spatial import distance
 
 class KNN:
     def __init__(self, K, ldata, method):
         self.K = K
         self.ldata = ldata
         self.method = method
-    def predict(self, tdata):
-        print("a")
+        self.data, self.target = np.array_split(self.ldata, [4], axis=1)
+        self.counter = 1
+
+
+
+
+    def predict(self, testdata):
+        self.testData, self.testTarget = np.array_split(testdata, [4], axis=1)
+        #print(self.testData)
+
+        for i in range(self.data.shape[0]):
+            for j in range(self.testData.shape[0]):
+
+                self.distances = distance.euclidean(self.data[i],self.testData[j])
+                self.counter = self.counter + 1
+                print("{0} --- {1} ---- {2} ---- {3}".format(self.counter, self.data[i],self.testData[j],self.distances))
+
+
     def score(self, tdata):
         print("b")
+
+
--- main.py
+++ main.py
@@ -1,9 +1,17 @@
-from utilities import loadTestingData,loadLearningData
 from knn import KNN
+import pandas as pd
+import numpy as np
+
 
 
 def main():
-    a = KNN(5,"iris.data.learning", "M")
+    ldata = np.array(pd.read_csv("Data/iris.data.learning"))
+    tdata = np.array(pd.read_csv("Data/iris.data.test"))
+    # print(ldata[:,0])  # print kolumne "0"
+
+
+    a = KNN(5,ldata, "E")
+    a.predict(tdata)
 
 if __name__ == '__main__':
     main()
