f325ae2b76122d229816f2ac4f68df6de7341acc
SmalecBartek <smalecbartlomiej@gmail.com>
2018-10-21 16:58:10 +0200

    Poprawiłem metode score

--- knn.py
+++ knn.py
@@ -1,11 +1,6 @@
-from os import terminal_size
-
-import pandas as pd
 import numpy as np
 from scipy.spatial import distance
-from scipy.stats import pearsonr
 
-from heapq import nsmallest
 
 class KNN:
     def __init__(self, K, ldata, method):
@@ -14,87 +9,93 @@ class KNN:
         self.method = method
         self.data, self.target = np.array_split(self.ldata, [4], axis=1)
         self.counter = 0
-        #self.a = 10
+        # self.a = 10
         self.plus = 0
         self.minus = 0
+        self.predictedTarget = []
+        #self.predictedTarget = np.empty(self.target.shape[0])
 
-    def setA(self,a):
-        self.a = a
-
-
-
-
+    def setIndex(self, a):
+        self.index = a
 
     def predictOne(self, testdata):
         self.testData, self.testTarget = np.array_split(testdata, [4], axis=1)
-        #print(self.testData)
-
-
-
+        # print(self.testData)
 
         self.distances = []
-
-
-
-
-
-
-
 
         for i in range(self.data.shape[0]):
             self.counter = self.counter + 1
 
-            self.distances.append(distance.euclidean(self.data[i], self.testData[self.a]))
+            self.distances.append(distance.euclidean(self.data[i], self.testData[self.index]))
 
+            # print(self.counter, self.distances)
 
-            #print(self.counter, self.distances)
-
-
-        #kopiuje całą tabele
+        # kopiuje całą tabele
         self.ldataLocal = self.ldata
-        self.distances =  np.asarray(self.distances)
+        self.distances = np.asarray(self.distances)
 
-        #dodaje kolumne w z dystansami w ldataLocal
-        self.ldataLocal = np.insert(self.ldataLocal,5,self.distances,axis=1)
+        # dodaje kolumne w z dystansami w ldataLocal
+        self.ldataLocal = np.insert(self.ldataLocal, 5, self.distances, axis=1)
 
-        #sortuje cały array wg kolumny 5
+        # sortuje cały array wg kolumny 5
         self.ldataLocal = sorted(self.ldataLocal, key=lambda a_entry: a_entry[5])
 
-        #po posortowaniu wybiera K wierszów o najmniejszym dystansie
+        # po posortowaniu wybiera K wierszów o najmniejszym dystansie
         kNajblizszychSasiadow = []
         for i in range(self.K):
             kNajblizszychSasiadow.append(self.ldataLocal[i][4])
-            #print(kNajblizszychSasiadow[i])
+            # print(kNajblizszychSasiadow[i])
 
-        #z kNajblizszychSasiadow wybiera najczesciej wysteu
+        # z kNajblizszychSasiadow wybiera najczesciej wystepujace
         def most_common(lista):
             return max(set(lista), key=lista.count)
 
+        '''''
         print("------------------------------------")
         print(most_common(kNajblizszychSasiadow))
-        print(self.testTarget[self.a])
-        print(most_common(kNajblizszychSasiadow) == self.testTarget[self.a] )
+        print(self.testTarget[self.index])
+        print(most_common(kNajblizszychSasiadow) == self.testTarget[self.index] )
+        '''''
 
 
+        #if most_common(kNajblizszychSasiadow) == self.testTarget[self.index]:
+            #self.plus = self.plus + 1
+        #else:
+            #self.minus = self.minus + 1
+        #print(self.plus / (self.plus + self.minus) * 100)
 
 
-        if most_common(kNajblizszychSasiadow) == self.testTarget[self.a]:
-            self.plus = self.plus + 1
-        else:
-            self.minus = self.minus + 1
+        return most_common(kNajblizszychSasiadow)
 
     def predict(self, testData):
         for i in range(testData.shape[0]):
-            print(i)
-            self.setA(i)
+            # print(i)
+            self.setIndex(i)
+            #np.append(self.predictedTarget,self.predictOne(testData) )
+            self.predictedTarget.append(self.predictOne(testData))
+        return self.predictedTarget
 
-            self.predictOne(testData)
+    def score(self, tdata, testTarget):
+        predictedTarget = self.predict(tdata)
+        predictedTarget = np.asarray(predictedTarget)
 
+        print(predictedTarget.shape)
+        print(self.testTarget.shape)
 
+        #print(predictedTarget[3])
+        #print(testTarget[:,0][3])
+        for i in range(testTarget.shape[0]):
 
+            print("Predicted: {0}, Correct value: {1}".format(predictedTarget[i],testTarget[:, 0][i]))
+            if predictedTarget[i] == testTarget[:,0][i]:
+                print("true")
+                self.plus =  self.plus + 1
+            else:
+                print("false")
+                self.minus = self.minus + 1
 
-
-    def score(self, tdata):
-        print("b")
+            print("----------------------------------------------")
+        return (self.plus/(self.plus + self.minus) * 100)
 
 
--- main.py
+++ main.py
@@ -1,30 +1,25 @@
-from knn import KNN
-import pandas as pd
 import numpy as np
+import pandas as pd
 
+from knn import KNN
 
 
 def main():
-
     ldata = np.array(pd.read_csv("Data/iris.data.learning", header=None))
     tdata = np.array(pd.read_csv("Data/iris.data.test", header=None))
 
-
-    # print(ldata[:,0])  # print kolumne "0"
-
+    testData, testTarget = np.array_split(tdata, [4], axis=1)
 
 
-    # popraw to a
-    a = KNN(2,ldata, "E")
-    a.predict(tdata)
-    #for i in range (tdata.shape[0]):
-        #print(i)
-        #a.setA(i)
-        #a.predict(tdata)
-    print(a.plus + a.minus)
-    print(a.plus/(a.plus + a.minus)*100)
+    a = KNN(2, ldata, "E")
+    #print(a.predict(tdata))
+    print(a.score(testData,testTarget))
+    #a.score(tdata,testTarget)
+
+
+
 
 if __name__ == '__main__':
     main()
 
-    # 5 virgi 7 veris 2 setosa
+
